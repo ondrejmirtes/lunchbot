16aec88bf91369e31a6a0b148571262107157b62
Jan Langer <jan.langer@gmail.com>
2016-05-04 22:45:38 +0200

    Fixed Cowshed parser

--- src/Provider/Kravin/KravinLunchMenuParser.php
+++ src/Provider/Kravin/KravinLunchMenuParser.php
@@ -17,7 +17,20 @@ class KravinLunchMenuParser
 		$crawler = new Crawler();
 		$crawler->addContent($html);
 
-		$soupText = trim(preg_replace('~\x{00a0}~siu', ' ', $crawler->filter('.entry-content table td')->text()));
+		$soupText = '';
+		if (count($crawler->filter('.entry-content table td')) > 0) {
+			$soupText = trim(preg_replace('~\x{00a0}~siu', ' ', $crawler->filter('.entry-content table td')->text()));
+		} else {
+			$nextLineIsSoup = false;
+			foreach ($crawler->filter('.entry-content p') as $soupCandidate) {
+				if (\Nette\Utils\Strings::lower(\Nette\Utils\Strings::trim($soupCandidate->textContent)) === 'polévka') {
+					$nextLineIsSoup = true;
+				} elseif ($nextLineIsSoup) {
+					$soupText = trim(preg_replace('~\x{00a0}~siu', ' ', $soupCandidate->textContent));
+					break;
+				}
+			}
+		}
 
 		$result = [
 			new LunchMenuItem($soupText),
